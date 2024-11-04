752be7e03d084c83a5e909910bc51f710c0db6e6
Esteban Porqueras <16k.ram@gmail.com>
2024-11-04 19:30:08 +0100

    Add files via upload

---
+++ productos.py
@@ -0,0 +1,5 @@
+productos={
+   "Model X": {"especificacions": "Pantalla OLED, 128GB, Càmera 12MP", "preu": 699.99},
+   "Model Y": {"especificacions": "Pantalla LCD, 64GB, Càmera 8MP", "preu": 499.99},
+   "Model Z": {"especificacions": "Pantalla AMOLED, 256GB, Càmera 16MP", "preu": 899.99}
+}
